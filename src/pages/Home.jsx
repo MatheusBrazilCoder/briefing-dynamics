4d7e3203e593ec2969f4b55921cef93ac8b0a1e3
MatheusBrazilCoder <matheusbrazil0324@gmail.com>
2024-10-28 00:23:00 -0300

    att

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import '../assets/css/home.css';
 import React from "react";
 import { Link } from "react-router-dom";
+// Importando os ícones
+import { FaLaptopCode } from "react-icons/fa";
+import { BiSolidPaintRoll } from "react-icons/bi";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 
 const Home = () =>{
     return(
@@ -14,9 +18,26 @@ const Home = () =>{
             <div className="home-nav__list">
                 <Link to="/briefing-sites" className="home-nav__item">
                     <div className="home-nav__container">
-                        <img src="" alt="" />
+                        <FaLaptopCode className="home-nav__icon" size={60} />
                         <h2 className="home-nav__title">
                             Criação de Sites
+                        </h2>
+                    </div>
+                </Link>
+                <Link to="/briefing-sites" className="home-nav__item">
+                    <div className="home-nav__container">
+                        <BiSolidPaintRoll className="home-nav__icon" size={60} />
+                        <h2 className="home-nav__title">
+                            Criação de Marca
+                        </h2>
+                    </div>
+                </Link>
+                <Link to="/briefing-sites" className="home-nav__item">
+                    <div className="home-nav__container">
+                        <AiOutlineFundProjectionScreen className="home-nav__icon" size={60} />
+                        <h2 className="home-nav__title">
+                            Gestão de 
+                            Tráfego Pago
                         </h2>
                     </div>
                 </Link>
